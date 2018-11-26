4c02d4282dc260a9be74aec3c4196c7171f56d90
swd3e2 <www.shinj@gmail.com>
2018-11-26 22:36:46 +0500

    1

--- Hollow Engine/Main.cpp
+++ Hollow Engine/Main.cpp
@@ -1,7 +1,27 @@
+#define NOMINMAX
 #include "windows.h"
+#include "Engine/Core/ECS/Memory/Allocators/PoolAllocator.h"
 
 int main()
 {
+	void * memory = malloc(1024);
+	ECS::Memory::Allocator::PoolAllocator allocator{ 512, memory, 36, sizeof(std::size_t)};
+	
+	void * mem = allocator.allocate(36, sizeof(std::size_t));
+	ZeroMemory(mem, 36);
+
+	mem = allocator.allocate(36, sizeof(std::size_t));
+	ZeroMemory(mem, 36);
+
+	mem = allocator.allocate(36, sizeof(std::size_t));
+	ZeroMemory(mem, 36);
+
+	mem = allocator.allocate(36, sizeof(std::size_t));
+	ZeroMemory(mem, 36);
+
+	mem = allocator.allocate(36, sizeof(std::size_t));
+	ZeroMemory(mem, 36);
+
 
 	system("pause");
 	return 0;
